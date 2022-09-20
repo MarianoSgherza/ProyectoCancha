5f8c16cc667e349b3fd319298005ffcf5d9a244e
MarianoSgherza <marianosgherza@gmail.com>
2022-09-19 22:20:30 -0300

    Agregar archivos de proyecto.

---
+++ ProyectoCancha/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ProyectoCancha
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
