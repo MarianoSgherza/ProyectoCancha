ac8e4a9b157801c8d6abe030f88c6929a81256c8
LeonelHidalgo <111834167+Lahkpom@users.noreply.github.com>
2022-10-17 11:53:35 -0300

    Update Program.cs

--- ProyectoCancha/Program.cs
+++ ProyectoCancha/Program.cs
@@ -10,6 +10,7 @@ namespace ProyectoCancha
             Console.ReadKey();
             //Prueba de modificacion desde vscode hidalgo
             //Prueba 2
+            //Prueba Modificacion desde editor de github
         }
     }
 }
