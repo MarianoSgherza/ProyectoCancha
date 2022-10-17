42a36c05cd6429e4d9e1048f5aed4e06d89e9bf2
LeonelHidalgo <111834167+Lahkpom@users.noreply.github.com>
2022-10-17 15:25:57 -0300

    Prueba 4

--- ProyectoCancha/Program.cs
+++ ProyectoCancha/Program.cs
@@ -11,6 +11,7 @@ namespace ProyectoCancha
             //Prueba de modificacion desde vscode hidalgo
             //Prueba 2
             //Prueba Modificacion desde editor de github
+            //Prueba Modificacion desde vscode en la nube de github
         }
     }
 }
