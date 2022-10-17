6a3e55c246fb320b30e9a2902db092058a9c730b
Leonel Hidalgo <118.hidalgo.leonel.1358@gmail.com>
2022-10-17 11:33:45 -0300

    Prueba desde vscode hidalgo

--- ProyectoCancha/Program.cs
+++ ProyectoCancha/Program.cs
@@ -8,8 +8,6 @@ namespace ProyectoCancha
         {
             Console.WriteLine("Hello World!");
             Console.ReadKey();
-
-            //Prueba de modifiación desde vscode (hidalgo)
         }
     }
 }
